53b3c19b676b7e0dbdf38f7b20b165187c53e630
pratkjn <pratkjn@gmail.com>
2021-07-29 14:50:55 +0530

    different type of selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]', //this will be work like an attribute/props i.e. <div app-servers></div>
+  // selector: '.app-servers', //this will be work like a class name to the element i.e. <div class='app-servers'></div>
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
