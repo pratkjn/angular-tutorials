07ae5ab08db1b9ac227da8b20803f47c2e44fda7
pratkjn <pratkjn@gmail.com>
2021-08-05 22:42:35 +0530

    $event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ export class ServersComponent implements OnInit {
 
 	allowNewServer = false;
 	serverCreationStatus = "No server was created";
-
+	serverName = ''
 	constructor() {
 		setTimeout(() => {
 			this.allowNewServer = true;
@@ -21,9 +21,13 @@ export class ServersComponent implements OnInit {
 	ngOnInit(): void {
 	}
 
-	onCreateSever(){
+	onCreateSever() {
 		console.log('in onCreateSever');
 		this.serverCreationStatus = 'Server was created';
+	}
+
+	onUpdateServerName(event: Event) {
+		this.serverName = (<HTMLInputElement>event.target).value;
 	}
 
 }
