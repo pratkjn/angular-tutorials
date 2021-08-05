e1f0e7f9e51db551641ad54f55b15793694517c3
pratkjn <pratkjn@gmail.com>
2021-08-05 22:09:57 +0530

    proprty binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  // selector: '[app-servers]', //this will be work like an attribute/props i.e. <div app-servers></div>
-  // selector: '.app-servers', //this will be work like a class name to the element i.e. <div class='app-servers'></div>
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+	selector: 'app-servers',
+	// selector: '[app-servers]', //this will be work like an attribute/props i.e. <div app-servers></div>
+	// selector: '.app-servers', //this will be work like a class name to the element i.e. <div class='app-servers'></div>
+	templateUrl: './servers.component.html',
+	styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+	allowNewServer = false;
 
-  ngOnInit(): void {
-  }
+	constructor() {
+		setTimeout(() => {
+			this.allowNewServer = true;
+		}, 2000);
+	}
+
+	ngOnInit(): void {
+	}
 
 }
