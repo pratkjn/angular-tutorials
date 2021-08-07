607836a650078fd6fb0968a0e6c5553a0c254601
pratkjn <pratkjn@gmail.com>
2021-08-08 00:24:34 +0530

    Recipe Book Ready

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent implements OnInit, OnChanges {
+  selectedRecipe?: Recipe;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('in changes', changes)
+  }
+
+  handleRecipe(recipe: Recipe) {
+    console.log('recipecdssdfsf', recipe)
+
   }
 
 }
