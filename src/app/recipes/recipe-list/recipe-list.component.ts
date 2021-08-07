2d25ef5e67ca6f5bc14344d2c7a8153051e356bc
pratkjn <pratkjn@gmail.com>
2021-08-07 13:57:38 +0530

    recipe List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Array<Recipe> = [
+    new Recipe('Test Recipe', 'This is test', 'https://image.shutterstock.com/image-photo/lady-reading-pizza-recipe-culinary-600w-1052789864.jpg')
+    ,new Recipe('Paneer Tikka', 'Tasty Paneer tikka recipe', 'https://image.shutterstock.com/image-photo/paneer-tikka-600w-623226842.jpg')
+  ];
 
   constructor() { }
 
