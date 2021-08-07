fa231b27578b712e096e1d51c8b9fa9a7ff8bd98
pratkjn <pratkjn@gmail.com>
2021-08-07 13:25:00 +0530

    header UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styles: ['./header.component.css']
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent {
